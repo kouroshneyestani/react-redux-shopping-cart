fc3c5e23fd4e77b4dcb6a0568984d9565761974b
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-10 03:55:00 -0700

    update(shopping-cart): refactor visibility management and add internal state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import Products from "./components/Products";
-import Cart from "./components/Cart";
+import { useState } from "react";
 
-import "./App.css";
+import Products from "./components/Products";
+import ShoppingCart from "./components/Cart";
 
 function App() {
+    const [isShoppingCartVisible, setIsShoppingCartVisible] = useState(false);
+
+    // Function to toggle the cart visibility
+    const toggleShoppingCart = () => {
+        setIsShoppingCartVisible(!isShoppingCartVisible);
+    };
+
     return (
         <>
             <div className="app">
                 <div>
-                    <h1>React Redux Shopping Cart</h1>
+                    <h1>
+                        React Redux Shopping Cart
+                        <button onClick={toggleShoppingCart}>
+                            Toogle cart
+                        </button>
+                    </h1>
                     <Products />
-                    <Cart />
+                    <ShoppingCart
+                        isVisible={isShoppingCartVisible}
+                        toggleCart={toggleShoppingCart}
+                    />
                 </div>
             </div>
         </>
--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux"; // Import useSelector to read state and useDispatch to send actions
 import {
     removeItem,
@@ -5,7 +6,7 @@ import {
     selectTotalPrice,
 } from "../../features/cart/cartSlice"; // Import actions for cart state
 
-const Cart = () => {
+const Cart = ({ isVisible, toggleCart }) => {
     const dispatch = useDispatch(); // Get the dispatch function to send actions
     const cartItems = useSelector((state) => state.cart.items); // Get the cart items from the Redux store
     const totalPrice = useSelector(selectTotalPrice); // Get the total price from the cart selector
@@ -21,35 +22,75 @@ const Cart = () => {
     };
 
     return (
-        <div>
-            <h2>Shopping Cart</h2>
-            <ul>
-                {cartItems.map((item) => (
-                    <li key={item.id}>
-                        {item.name} - ${item.price} x {item.quantity}
-                        <button onClick={() => handleRemove(item.id)}>
-                            Remove
+        <div
+            className={`w-full h-screen fixed bg-white z-50 overflow-hidden before:top-0 before:left-0 before:bg-stone-900 before:fixed before:w-full before:h-screen before:opacity-10 text-sm ${
+                isVisible
+                    ? "pointer-events-auto"
+                    : "opacity-0 pointer-events-none"
+            }`}
+        >
+            <div
+                className={`w-[340px] max-w-[90%] h-screen top-0 right-0 fixed bg-white overflow-x-hidden transition-all ${
+                    isVisible ? "translate-x-0" : "translate-x-full"
+                }`}
+            >
+                <header className="h-16 flex items-center justify-between px-6 bg-gray-100">
+                    <span className="text-md">Shopping Bag</span>
+                    <button className="w-8 h-8" onClick={toggleCart}>
+                        x
+                    </button>
+                </header>
+                <article className="bg-blue-100 pb-300 overflow-x-auto">
+                    <ul>
+                        {cartItems.map((item) => (
+                            <li key={item.id}>
+                                {item.name} - ${item.price} x {item.quantity}
+                                <button onClick={() => handleRemove(item.id)}>
+                                    Remove
+                                </button>
+                                <button
+                                    onClick={() =>
+                                        handleQuantityChange(
+                                            item.id,
+                                            item.quantity + 1
+                                        )
+                                    }
+                                >
+                                    Increase Quantity
+                                </button>
+                                <button
+                                    onClick={() =>
+                                        handleQuantityChange(
+                                            item.id,
+                                            item.quantity - 1
+                                        )
+                                    }
+                                    disabled={item.quantity <= 1}
+                                >
+                                    Decrease Quantity
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </article>
+                <footer className="w-full left-0 bottom-0 absolute flex flex-col gap-4 px-6 py-6">
+                    <div className="flex items-center justify-between">
+                        <span>SUBTOTAL</span>
+                        <div>
+                            <span>$</span>
+                            <span>{totalPrice.toFixed(2)}</span>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <button className="w-full h-10 bg-gray-200 hover:bg-gray-300 flex items-center justify-center">
+                            <span>View Cart</span>
                         </button>
-                        <button
-                            onClick={() =>
-                                handleQuantityChange(item.id, item.quantity + 1)
-                            }
-                        >
-                            Increase Quantity
+                        <button className="w-full h-10 bg-black hover:bg-black flex items-center justify-center text-white">
+                            <span>Checkout</span>
                         </button>
-                        <button
-                            onClick={() =>
-                                handleQuantityChange(item.id, item.quantity - 1)
-                            }
-                            disabled={item.quantity <= 1}
-                        >
-                            Decrease Quantity
-                        </button>
-                    </li>
-                ))}
-            </ul>
-            {/* Display the total price */}
-            <h3>Total Price: ${totalPrice.toFixed(2)}</h3>
+                    </div>
+                </footer>
+            </div>
         </div>
     );
 };
