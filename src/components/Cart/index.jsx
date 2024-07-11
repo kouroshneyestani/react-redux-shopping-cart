6a6eb82d06873462c73313a16e96bfdb7ac43281
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-11 08:34:53 -0700

    refactor(shopping-cart): extract eepeated SVG to a separate component

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -5,6 +5,29 @@ import { withCartTotal } from "../../hocs/";
 import { formatPrice } from "../../utils/formatPrice";
 
 /**
+ * Close Icon component.
+ * @returns {JSX.Element} - The Close Icon component.
+ */
+const CloseIcon = ({ width = 16, height = 16 }) => (
+    <svg
+        width={width}
+        height={height}
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M0.414336 14.1421L14.5565 0L15.9707 1.41421L1.82855 15.5563L0.414336 14.1421Z"
+            fill="#000"
+        />
+        <path
+            d="M1.41421 0.142113L15.5563 14.2842L14.1421 15.6985L0 1.55633L1.41421 0.142113Z"
+            fill="#000"
+        />
+    </svg>
+);
+
+/**
  * Cart component displaying items in the shopping cart.
  * @param {Object} props
  * @param {number} props.totalPrice - The total price of the items in the cart.
@@ -52,22 +75,7 @@ const Cart = ({ totalPrice }) => {
                         className="w-4 h-4 flex items-center justify-center cursor-pointer"
                         onClick={toggleCart}
                     >
-                        <svg
-                            width="16"
-                            height="16"
-                            viewBox="0 0 16 16"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M0.414336 14.1421L14.5565 0L15.9707 1.41421L1.82855 15.5563L0.414336 14.1421Z"
-                                fill="#000"
-                            />
-                            <path
-                                d="M1.41421 0.142113L15.5563 14.2842L14.1421 15.6985L0 1.55633L1.41421 0.142113Z"
-                                fill="#000"
-                            />
-                        </svg>
+                        <CloseIcon />
                     </button>
                 </header>
                 <article className="py-6 px-6 overflow-x-auto">
@@ -125,22 +133,10 @@ const Cart = ({ totalPrice }) => {
                                                                 )
                                                             }
                                                         >
-                                                            <svg
-                                                                width="8"
-                                                                height="8"
-                                                                viewBox="0 0 16 16"
-                                                                fill="none"
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                            >
-                                                                <path
-                                                                    d="M0.414336 14.1421L14.5565 0L15.9707 1.41421L1.82855 15.5563L0.414336 14.1421Z"
-                                                                    fill="#000"
-                                                                />
-                                                                <path
-                                                                    d="M1.41421 0.142113L15.5563 14.2842L14.1421 15.6985L0 1.55633L1.41421 0.142113Z"
-                                                                    fill="#000"
-                                                                />
-                                                            </svg>
+                                                            <CloseIcon
+                                                                width={8}
+                                                                height={8}
+                                                            />
                                                         </button>
                                                     </div>
                                                     <div className="gap-4 flex items-center justify-between">
