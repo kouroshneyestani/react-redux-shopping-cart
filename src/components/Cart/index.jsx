f99b3d73868c1204c093b84fc59bd1c92cc964eb
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-11 08:12:25 -0700

    refactor(shopping-cart): improve shopping cart component

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,27 +1,51 @@
-// src/components/Cart.jsx
-import { useShoppingCart } from "../../contexts/ShoppingCartContext";
-import { useSelector, useDispatch } from "react-redux"; // Import useSelector to read state and useDispatch to send actions
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useCart, useCartActions, useProductSearch } from "../../hooks/index";
+import { withCartTotal } from "../../hocs/";
 import {
     removeItem,
     updateQuantity,
     selectTotalPrice,
-} from "../../features/cart/cartSlice"; // Import actions for cart state
+} from "../../features/cart/cartSlice";
+import { formatPrice } from "../../utils/formatPrice";
+import { fetchCartDetails } from "../../services/cartService";
 
-const Cart = () => {
-    const { isVisible, toggleCart } = useShoppingCart();
-    const dispatch = useDispatch(); // Get the dispatch function to send actions
-    const cartItems = useSelector((state) => state.cart.items); // Get the cart items from the Redux store
-    const totalPrice = useSelector(selectTotalPrice); // Get the total price from the cart selector
+/**
+ * Cart component displaying items in the shopping cart.
+ * @param {Object} props
+ * @param {number} props.totalPrice - The total price of the items in the cart.
+ * @returns {JSX.Element} - The Cart component.
+ */
+const Cart = ({ totalPrice }) => {
+    const { isVisible, toggleCart } = useCart(); // Use the custom cart hook for visibility
+    const dispatch = useDispatch();
+    const cartItems = useSelector((state) => state.cart.items); // Get cart items from Redux store
+    const { searchTerm, updateSearchTerm } = useProductSearch(); // Hook for product search
 
-    // Handler function to remove an item from the cart
+    // Actions from custom hook
+    const { removeFromCart, updateCartItem } = useCartActions();
+
+    // Handler to remove an item
     const handleRemove = (id) => {
-        dispatch(removeItem({ id })); // Remove the item from the cart
+        removeFromCart(id);
     };
 
-    // Handler function to update the quantity of an item in the cart
+    // Handler to update quantity
     const handleQuantityChange = (id, quantity) => {
-        dispatch(updateQuantity({ id, quantity })); // Update the item's quantity
+        updateCartItem(id, quantity);
     };
+
+    // Handler to search products
+    const handleSearch = (event) => {
+        updateSearchTerm(event.target.value);
+    };
+
+    useEffect(() => {
+        // Fetch cart details
+        fetchCartDetails().then((data) => {
+            console.log("Cart details:", data);
+        });
+    }, []);
 
     return (
         <div
@@ -61,105 +85,126 @@ const Cart = () => {
                     </button>
                 </header>
                 <article className="py-6 px-6 overflow-x-auto">
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={handleSearch}
+                        placeholder="Search products"
+                        className="mb-4 p-2 border border-gray-300 rounded"
+                    />
                     <ul className="flex flex-col gap-4">
-                        {!cartItems.length ? (
+                        {cartItems.length > 0 ? (
+                            cartItems
+                                .filter((item) =>
+                                    item.name
+                                        .toLowerCase()
+                                        .includes(searchTerm.toLowerCase())
+                                )
+                                .map((item) => (
+                                    <li
+                                        key={item.id}
+                                        className="[&:not(:last-child)]:border-b-[1px] border-solid border-gray-200 pb-4"
+                                    >
+                                        <div className="relative">
+                                            <div className="gap-4 flex justify-between">
+                                                <div>
+                                                    <div className="w-[90px] h-[90px] bg-gray-300"></div>
+                                                </div>
+                                                <div className="w-full flex flex-col gap-2">
+                                                    <div className="flex flex-col pt-2">
+                                                        <h6>{item.name}</h6>
+                                                        <ul className="gap-1 flex items-center text-xs">
+                                                            <li>
+                                                                <span>
+                                                                    Size:{" "}
+                                                                </span>
+                                                                <span>XL</span>,
+                                                            </li>
+                                                            <li>
+                                                                <span>
+                                                                    Color:{" "}
+                                                                </span>
+                                                                <span>
+                                                                    Blue
+                                                                </span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    <div className="gap-4 flex items-center justify-between">
+                                                        <div className="gap-1 flex items-center">
+                                                            <button
+                                                                className="w-6 h-6 flex items-center justify-center cursor-pointer"
+                                                                onClick={() =>
+                                                                    handleQuantityChange(
+                                                                        item.id,
+                                                                        item.quantity -
+                                                                            1
+                                                                    )
+                                                                }
+                                                                disabled={
+                                                                    item.quantity <=
+                                                                    1
+                                                                }
+                                                            >
+                                                                -
+                                                            </button>
+                                                            <span>
+                                                                {item.quantity}
+                                                            </span>
+                                                            <button
+                                                                className="w-6 h-6 flex items-center justify-center cursor-pointer"
+                                                                onClick={() =>
+                                                                    handleQuantityChange(
+                                                                        item.id,
+                                                                        item.quantity +
+                                                                            1
+                                                                    )
+                                                                }
+                                                            >
+                                                                +
+                                                            </button>
+                                                        </div>
+                                                        <div>
+                                                            <span>$</span>
+                                                            <span className="text-lg">
+                                                                {formatPrice(
+                                                                    item.price
+                                                                )}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button
+                                                className="top-2 right-0 w-4 h-4 flex items-center justify-center absolute cursor-pointer"
+                                                onClick={() =>
+                                                    handleRemove(item.id)
+                                                }
+                                            >
+                                                <svg
+                                                    width="8"
+                                                    height="8"
+                                                    viewBox="0 0 16 16"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        d="M0.414336 14.1421L14.5565 0L15.9707 1.41421L1.82855 15.5563L0.414336 14.1421Z"
+                                                        fill="#000"
+                                                    />
+                                                    <path
+                                                        d="M1.41421 0.142113L15.5563 14.2842L14.1421 15.6985L0 1.55633L1.41421 0.142113Z"
+                                                        fill="#000"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </li>
+                                ))
+                        ) : (
                             <p className="text-center">
                                 Your cart is empty. Start shopping!
                             </p>
-                        ) : (
-                            cartItems.map((item) => (
-                                <li
-                                    key={item.id}
-                                    className="[&:not(:last-child)]:border-b-[1px] border-solid border-gray-200 pb-4"
-                                >
-                                    <div className="relative">
-                                        <div className="gap-4 flex justify-between">
-                                            <div>
-                                                <div className="w-[90px] h-[90px] bg-gray-300"></div>
-                                            </div>
-                                            <div className="w-full flex flex-col gap-2">
-                                                <div className="flex flex-col pt-2">
-                                                    <h6>{item.name}</h6>
-                                                    <ul className="gap-1 flex items-center text-xs">
-                                                        <li>
-                                                            <span>Size: </span>
-                                                            <span>XL</span>,
-                                                        </li>
-                                                        <li>
-                                                            <span>Color: </span>
-                                                            <span>Blue</span>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                                <div className="gap-4 flex items-center justify-between">
-                                                    <div className="gap-1 flex items-center">
-                                                        <button
-                                                            className="w-6 h-6 flex items-center justify-center cursor-pointer"
-                                                            onClick={() =>
-                                                                handleQuantityChange(
-                                                                    item.id,
-                                                                    item.quantity -
-                                                                        1
-                                                                )
-                                                            }
-                                                            disabled={
-                                                                item.quantity <=
-                                                                1
-                                                            }
-                                                        >
-                                                            -
-                                                        </button>
-                                                        <span>
-                                                            {item.quantity}
-                                                        </span>
-                                                        <button
-                                                            className="w-6 h-6 flex items-center justify-center cursor-pointer"
-                                                            onClick={() =>
-                                                                handleQuantityChange(
-                                                                    item.id,
-                                                                    item.quantity +
-                                                                        1
-                                                                )
-                                                            }
-                                                        >
-                                                            +
-                                                        </button>
-                                                    </div>
-                                                    <div>
-                                                        <span>$</span>
-                                                        <span className="text-lg">
-                                                            {item.price}.00
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <button
-                                            className="top-2 right-0 w-4 h-4 flex items-center justify-center absolute cursor-pointer"
-                                            onClick={() =>
-                                                handleRemove(item.id)
-                                            }
-                                        >
-                                            <svg
-                                                width="8"
-                                                height="8"
-                                                viewBox="0 0 16 16"
-                                                fill="none"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path
-                                                    d="M0.414336 14.1421L14.5565 0L15.9707 1.41421L1.82855 15.5563L0.414336 14.1421Z"
-                                                    fill="#000"
-                                                />
-                                                <path
-                                                    d="M1.41421 0.142113L15.5563 14.2842L14.1421 15.6985L0 1.55633L1.41421 0.142113Z"
-                                                    fill="#000"
-                                                />
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </li>
-                            ))
                         )}
                     </ul>
                 </article>
@@ -185,4 +230,5 @@ const Cart = () => {
     );
 };
 
-export default Cart;
+// Enhance the Cart component with HOCs
+export default withCartTotal(Cart);
