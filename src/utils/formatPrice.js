8f8a05e351eb00f79502970a8b6ea8833856d6bd
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-11 07:57:40 -0700

    feat: add formatPrice

---
+++ src/utils/formatPrice.js
@@ -0,0 +1,8 @@
+/**
+ * Formats a price to a string with two decimal places.
+ * @param price - The price to format.
+ * @returns - The formatted price string.
+ */
+export const formatPrice = (price) => {
+    return price.toFixed(2);
+};
