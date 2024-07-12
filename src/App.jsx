b08a13eb23fb90e31f06a363c25822961878c58a
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-12 08:41:48 -0700

    chore: remove ShoppingCartContext and ShoppingCartProvider as Redux is used for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 // src/App.jsx
 import React from "react";
-import { ShoppingCartProvider } from "./contexts/ShoppingCartContext"; // Import ShoppingCartProvider
 import Products from "./components/Products";
 import ShoppingCart from "./components/Cart";
 import CartToggleButton from "./components/CartToggleButton"; // Import CartToggleButton component
 
 function App() {
     return (
-        <ShoppingCartProvider>
-            <div>
-                <h1>React Redux Shopping Cart</h1>
-                <CartToggleButton>[ CART ]</CartToggleButton>
-                <Products />
-                <ShoppingCart />
-            </div>
-        </ShoppingCartProvider>
+        <div>
+            <h1>React Redux Shopping Cart</h1>
+            <CartToggleButton>[ CART ]</CartToggleButton>
+            <Products />
+            <ShoppingCart />
+        </div>
     );
 }
 
