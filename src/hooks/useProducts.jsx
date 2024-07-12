08351b70f4cae772f2ee6886fe9792ae4ed5f79c
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-12 08:07:44 -0700

    feat: add useProducts hook, add new products

---
+++ src/hooks/useProducts.jsx
@@ -0,0 +1,32 @@
+// src/hooks/useProducts.js
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../services/productService";
+
+/**
+ * Custom hook for fetching products.
+ * @returns {Object} - An object containing products, loading state, and error.
+ */
+const useProducts = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetchProducts();
+                setProducts(data);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return { products, loading, error };
+};
+
+export default useProducts;
