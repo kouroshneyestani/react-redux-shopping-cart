0c54242f5a0f207d791a350bd3f1719b31905a1a
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-12 08:38:30 -0700

    refactor(hook): update useCart to manage cart visibility and search term with Redux

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,11 +1,23 @@
-import { useContext } from "react";
-import { ShoppingCartContext } from "../contexts/ShoppingCartContext";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleCart, selectIsCartOpen } from "../features/cart/cartSlice";
 
 /**
  * Custom hook for managing cart visibility and toggle functionality.
  * @returns {{ isVisible: boolean, toggleCart: () => void }}
  */
 export const useCart = () => {
-    const { isVisible, toggleCart } = useContext(ShoppingCartContext);
+    // Get the dispatch function from Redux
+    const dispatch = useDispatch();
+
+    // Get the cart open/close state from the Redux store
+    const isVisible = useSelector(selectIsCartOpen);
+
+    /**
+     * Toggles the visibility of the shopping cart.
+     */
+    const toggleCart = () => {
+        dispatch(toggleCart());
+    };
+
     return { isVisible, toggleCart };
 };
