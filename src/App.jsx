17d11c4141b2b24423bfb918f37cdead800dec5b
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-13 18:08:11 +0330

    feat: add shoppingCart in App router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Pages
 import Products from "./pages/Products";
+
+// Components
+import Cart from "./components/Cart";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/products" element={<Products />} />
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/products" element={<Products />} />
 
-                <Route
-                    path="*"
-                    element={
-                        <div className="text-center">404 - Page Not Found</div>
-                    }
-                />
-            </Routes>
-        </Router>
+                    <Route
+                        path="*"
+                        element={
+                            <div className="text-center">
+                                404 - Page Not Found
+                            </div>
+                        }
+                    />
+                </Routes>
+                
+                <Cart />
+            </Router>
+        </>
     );
 }
 
